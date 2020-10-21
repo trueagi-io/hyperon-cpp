488543969d12ef40a29d17148c15c60ea4a8a782
Vitaly Bogdanov <vsbogd@gmail.com>
2020-10-21 15:04:57 +0300

    Add SampleTextSpace with predefined syntax

--- python/tests/common.py
+++ python/tests/common.py
@@ -49,6 +49,23 @@ class MatchAtom(GroundedAtom):
     def __repr__(self):
         return "match"
 
+class UnaryOpAtom(GroundedAtom):
+
+    def __init__(self, name, op):
+        GroundedAtom.__init__(self)
+        self.name = name
+        self.op = op
+
+    def execute(self, args, result):
+        a = args.get_content()[1]
+        result.add_atom(ValueAtom(self.op(a.value)));
+
+    def __eq__(self, other):
+        return isinstance(other, BinaryOpAtom) and self.name == self.name
+
+    def __repr__(self):
+        return self.name
+
 class BinaryOpAtom(GroundedAtom):
 
     def __init__(self, name, op):
@@ -75,7 +92,7 @@ class MulAtom(BinaryOpAtom):
     def __init__(self):
         BinaryOpAtom.__init__(self, "*", lambda a, b: a * b)
 
-class PlusAtom(BinaryOpAtom):
+class AddAtom(BinaryOpAtom):
     def __init__(self):
         BinaryOpAtom.__init__(self, "+", lambda a, b: a + b)
 
@@ -94,3 +111,41 @@ class GreaterAtom(BinaryOpAtom):
 class LessAtom(BinaryOpAtom):
     def __init__(self):
         BinaryOpAtom.__init__(self, "<", lambda a, b: a < b)
+
+class OrAtom(BinaryOpAtom):
+    def __init__(self):
+        BinaryOpAtom.__init__(self, "or", lambda a, b: a or b)
+
+class AndAtom(BinaryOpAtom):
+    def __init__(self):
+        BinaryOpAtom.__init__(self, "and", lambda a, b: a and b)
+
+class NotAtom(BinaryOpAtom):
+    def __init__(self):
+        BinaryOpAtom.__init__(self, "not", lambda a: not a)
+
+class SampleTextSpace(TextSpace):
+
+    def __init__(self):
+        TextSpace.__init__(self)
+        self.register_token("\+", lambda token: AddAtom())
+        self.register_token("-", lambda token: SubAtom())
+        self.register_token("\*", lambda token: MulAtom())
+        self.register_token("\/", lambda token: DivAtom())
+        self.register_token("==", lambda token: EqualAtom())
+        self.register_token("<", lambda token: LessAtom())
+        self.register_token(">", lambda token: GreaterAtom())
+        self.register_token("or", lambda token: OrAtom())
+        self.register_token("and", lambda token: AndAtom())
+        self.register_token("not", lambda token: NotAtom())
+        self.register_token("\\d+(.\\d+)", lambda token: ValueAtom(float(token)))
+        self.register_token("\\d+", lambda token: ValueAtom(int(token)))
+        self.register_token("'[^']*'", lambda token: ValueAtom(str(token)))
+        self.register_token("True|False", lambda token: ValueAtom(token == 'True'))
+
+def atomese(program):
+    kb = GroundingSpace()
+    text = SampleTextSpace()
+    text.add_string(program)
+    kb.add_from_space(text)
+    return kb
